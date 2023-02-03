d9fe01d38568ddbc33ef44e636aac839c7821b8e
iamdeepak42 <iamdeepak42@gmail.com>
2023-02-02 23:19:45 -0600

    abebooks

--- webscraper/spiders/abebooks.py
+++ webscraper/spiders/abebooks.py
@@ -12,39 +12,43 @@ header = Headers(browser="chrome",  # Generate only Chrome UA
 
 
 
-class ebay_top3(scrapy.Spider):
-    name = 'ebay_top3'
+class abebooks(scrapy.Spider):
+    name = 'abebooks'
     custom_settings = {'CONCURRENT_REQUESTS': 1,
                        'FEED_FORMAT': 'csv',
-                       'FEED_URI': datetime.now().strftime('%Y_%m_%d__%H_%M') + 'ebay.csv',
+                       'FEED_URI': datetime.now().strftime('%Y_%m_%d__%H_%M') + 'abebooks.csv',
                        'RETRY_TIMES': 15,
                        'COOKIES_ENABLED': False,
                        'FEED_EXPORT_ENCODING' : "utf-8"
     }
     headers = {
-        'authority': 'www.ebay.com',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
+        'authority': 'www.abebooks.co.uk',
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'sec-gpc': '1',
-        'sec-fetch-site': 'none',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-user': '?1',
-        'sec-fetch-dest': 'document',
         'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'max-age=0',
+        'sec-ch-ua': '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Linux"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
     }
+
 
     proxy = 'http://xavigv:GOkNQBPK2DplRGqw_country-UnitedKingdom@proxy.packetstream.io:31112'
 
     def __init__(self, url=None, *args, **kwargs):
-        super(ebay_top3, self).__init__(*args, **kwargs)
+        super(abebooks, self).__init__(*args, **kwargs)
         self.url = url
 
    
     # file = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTeZxZduOfcazmDjKEGfYmHpJD1J1BGODjyAF91v8DMRMgR5fZQc9CAUPXuTQQMMAQHNyxTKTsLce04/pub?gid=0&single=true&output=csv'
     # url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTeZxZduOfcazmDjKEGfYmHpJD1J1BGODjyAF91v8DMRMgR5fZQc9CAUPXuTQQMMAQHNyxTKTsLce04/pub?gid=0&single=true&output=csv'
     
-    # url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR7IVO_B95bONHJX6ahtm9K8INz0Tt1bajd9EiqCYfpHk7b68UmlxvXxe7Gw1dX6EuSVFPhc8fhM4NI/pub?gid=0&single=true&output=csv'
+    # url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRtbluXDoMi--POZYgVYruqgotcT7PT2mbVi4Ic2Rf87ycyQ6b1gmzdT_1I41IHoHPqVsM_2V-nAic4/pub?gid=0&single=true&output=csv'
     def start_requests(self):
 
         df = pd.read_csv(self.url)
@@ -56,30 +60,22 @@ class ebay_top3(scrapy.Spider):
        
 
     def parse(self, response):
-
-        listings = response.xpath('//ul//div[@class="s-item__wrapper clearfix"]')[:3]
+        listings = response.xpath('//li[@data-cy="listing-item"]')[:3]
 
         for rank, listing in enumerate(listings):
 
-            title = listing.xpath('.//span[@role="heading"]/text()').get('')
+            title = listing.xpath('.//span[@data-cy="listing-title"]/text()').get('')
 
-            price = listing.xpath('.//span[@class="s-item__price"]/text()').get('')
+            price = listing.xpath('.//meta[@itemprop="price"]/@content').get('')
 
-            image = listing.xpath('.//div[contains(@class,"s-item__image")]//img/@src').get('')
+            isbn = listing.xpath('.//meta[@itemprop="isbn"]/@content').get('')
 
-            image = image.replace('s-l225.webp', 's-l500.jpg')
+            seller_name = listing.xpath('.//a[@data-cy="listing-seller-link"]/text()').get('')
 
-            shipping_cost = listing.xpath('.//span[@class="s-item__shipping s-item__logisticsCost"]/text()').get('')
-            if not shipping_cost:
-                shipping_cost = listing.xpath('.//span[@class="s-item__dynamic s-item__freeXDays"]//text()').get('')
-            try:
-                item_number = listing.xpath('.//a/@href').get('').split('/itm/')[1].split('?')[0]
-            except:
-                item_number = ''
-            seller_name = listing.xpath('.//*[@class="s-item__seller-info-text"]//text()').get('').split('(')[0]
+            shipping_cost = listing.xpath('.//span[contains(@id,"item-shipping-price-")]/text()').get('')
 
-            if seller_name:
-                seller_name = seller_name.strip()
+            image = listing.xpath('.//div[@data-cy="listing-image"]/img/@src').get('')
+            
             yield {
                 'URL': response.url,
                 'Image URL': image,
@@ -87,7 +83,7 @@ class ebay_top3(scrapy.Spider):
                 'Product Price': price,
                 'Shipping Fee': shipping_cost,
                 'Postion': rank + 1,
-                'Item Number': item_number,
+                'ISBN': isbn,
                 'Seller Name': seller_name,
 
 
