5db553c199a24dc8d99413ad3fe7f4407a08ecc1
xavibcn1999 <75781897+xavibcn1999@users.noreply.github.com>
2024-06-28 20:16:28 +0100

    Update abebooks.py

--- webscraper/spiders/abebooks.py
+++ webscraper/spiders/abebooks.py
@@ -37,7 +37,7 @@ class abebooks(scrapy.Spider):
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
     }
     proxy_list = [
-        'http://xavigv:ee3ee0580b725494_country-UnitedKingdom@proxy.packetstream.io:31112',
+        'http://xavigv:ee3ee0580b725494@proxy.packetstream.io:31112',
         # Add more proxies here
     ]
 
