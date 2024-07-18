55710f98c9f20c3bee410739828e321f1afd9f45
xavibcn1999 <75781897+xavibcn1999@users.noreply.github.com>
2024-07-18 10:35:02 +0100

    Update settings.py

--- webscraper/settings.py
+++ webscraper/settings.py
@@ -89,3 +89,14 @@ ROBOTSTXT_OBEY = False
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
 INPUT_FILE = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQtb-UKr49Cj5tAof845aBQnN6Z_fnfaeGvvfKjee-XWz2OmNFlqW-XkItXUkhjEt7Xnr50yGdu_wcC/pub?output=csv'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Xavidatabase',
+        'USER': 'xaviopen',
+        'PASSWORD': '8ww5b2v1r8yomya',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
+
