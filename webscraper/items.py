7b08738ed9e821865ff2eb981b4d6fa5e9935d53
xavibcn1999 <75781897+xavibcn1999@users.noreply.github.com>
2024-07-22 21:27:11 +0100

    Update items.py

--- webscraper/items.py
+++ webscraper/items.py
@@ -2,7 +2,8 @@ import scrapy
 
 class WebscraperItem(scrapy.Item):
     ASIN = scrapy.Field()
-    Image_URL = scrapy.Field()
-    Product_Title = scrapy.Field()
-    Product_Price = scrapy.Field()
-    Shipping_Fee = scrapy.Field()
+    image_url = scrapy.Field()  # Ensure this matches the field name used in the spider
+    product_title = scrapy.Field()
+    product_price = scrapy.Field()
+    shipping_fee = scrapy.Field()
+
