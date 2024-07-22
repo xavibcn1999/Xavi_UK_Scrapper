d3dd8322cf89cabd0e420b977d12022f8d48196a
xavibcn1999 <75781897+xavibcn1999@users.noreply.github.com>
2024-07-22 11:08:52 +0100

    Update settings.py

--- webscraper/settings.py
+++ webscraper/settings.py
@@ -1,8 +1,11 @@
-# settings.py
-
-ITEM_PIPELINES = {
-    'your_project.pipelines.MongoDBPipeline': 300,
+INPUT_FILE = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQtb-UKr49Cj5tAof845aBQnN6Z_fnfaeGvvfKjee-XWz2OmNFlqW-XkItXUkhjEt7Xnr50yGdu_wcC/pub?output=csv'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Xavidatabase',
+        'USER': 'xaviopen',
+        'PASSWORD': '8ww5b2v1r8yomya',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
 }
-
-MONGO_URI = 'mongodb+srv://xavidb:superman123@serverlessinstance0.lih2lnk.mongodb.net/'
-MONGO_DATABASE = 'Xavi_UK'
