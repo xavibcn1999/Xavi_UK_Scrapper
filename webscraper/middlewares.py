0da206e14586dc0244dffe0a77d099a83fc220c9
xavibcn1999 <75781897+xavibcn1999@users.noreply.github.com>
2024-07-22 10:09:36 +0100

    Update middlewares.py

--- webscraper/middlewares.py
+++ webscraper/middlewares.py
@@ -1,50 +1,13 @@
-from scrapy import signals
-from itemadapter import is_item, ItemAdapter
+import time
+from scrapy.downloadermiddlewares.retry import RetryMiddleware
+from scrapy.utils.response import response_status_message
 
-class WebscraperSpiderMiddleware:
-    @classmethod
-    def from_crawler(cls, crawler):
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
-
-    def process_spider_input(self, response, spider):
-        return None
-
-    def process_spider_output(self, response, result, spider):
-        for i in result:
-            yield i
-
-    def process_spider_exception(self, response, exception, spider):
-        pass
-
-    def process_start_requests(self, start_requests, spider):
-        for r in start_requests:
-            yield r
-
-    def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
-
-
-class WebscraperDownloaderMiddleware:
-    @classmethod
-    def from_crawler(cls, crawler):
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
-
-    def process_request(self, request, spider):
-        # Configura el proxy para cada solicitud
-        proxy = "http://xavigv:e8qcHlJ5jdHxl7Xj_country-UnitedKingdom@proxy.packetstream.io:31112"
-        request.meta['proxy'] = proxy
-        return None
-
+class CustomRetryMiddleware(RetryMiddleware):
     def process_response(self, request, response, spider):
+        if response.status in self.retry_http_codes:
+            reason = response_status_message(response.status)
+            retry_times = request.meta.get('retry_times', 0)
+            wait_time = 2 ** retry_times  # Exponential backoff
+            time.sleep(wait_time)
+            return self._retry(request, reason, spider) or response
         return response
-
-    def process_exception(self, request, exception, spider):
-        spider.logger.error(f'Error processing request {request}: {exception}')
-        pass
-
-    def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
