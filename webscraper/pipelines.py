507bc10042336f47ae6e2663e524e6aa77bb9d41
xavibcn1999 <75781897+xavibcn1999@users.noreply.github.com>
2024-07-22 21:22:47 +0100

    Update pipelines.py

--- webscraper/pipelines.py
+++ webscraper/pipelines.py
@@ -1,27 +1,36 @@
+# pipelines.py
 import pymongo
+from scrapy.exceptions import DropItem
 
 class MongoDBPipeline:
-
-    def __init__(self, mongo_uri, mongo_db, mongo_collection):
+    def __init__(self, mongo_uri, mongo_db):
         self.mongo_uri = mongo_uri
         self.mongo_db = mongo_db
-        self.mongo_collection = mongo_collection
 
     @classmethod
     def from_crawler(cls, crawler):
         return cls(
             mongo_uri=crawler.settings.get('MONGO_URI'),
-            mongo_db=crawler.settings.get('MONGO_DATABASE'),
-            mongo_collection=crawler.settings.get('MONGODB_COLLECTION', 'items')
+            mongo_db=crawler.settings.get('MONGO_DATABASE', 'items')
         )
 
     def open_spider(self, spider):
         self.client = pymongo.MongoClient(self.mongo_uri)
         self.db = self.client[self.mongo_db]
+        self.collection = self.db['Search_uk_E']
 
     def close_spider(self, spider):
         self.client.close()
 
     def process_item(self, item, spider):
-        self.db[self.mongo_collection].insert_one(dict(item))
+        self.collection.update_one(
+            {'ASIN': item['ASIN']},
+            {'$set': {
+                'Image URL': item['Image_URL'],
+                'Product Title': item['Product_Title'],
+                'Product Price': item['Product_Price'],
+                'Shipping Fee': item['Shipping_Fee']
+            }},
+            upsert=True
+        )
         return item
