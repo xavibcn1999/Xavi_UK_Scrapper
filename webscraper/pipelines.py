be19bd6fdec7849f0bd058934e16d65e81e5908b
xavibcn1999 <75781897+xavibcn1999@users.noreply.github.com>
2024-07-22 11:15:33 +0100

    Update pipelines.py

--- webscraper/pipelines.py
+++ webscraper/pipelines.py
@@ -1,86 +1,40 @@
-# Scrapy settings for webscraper project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+# webscraper/pipelines.py
 
-BOT_NAME = 'webscraper'
+from pymongo import MongoClient
 
-SPIDER_MODULES = ['webscraper.spiders']
-NEWSPIDER_MODULE = 'webscraper.spiders'
+class MongoDBPipeline:
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'webscraper (+http://www.yourdomain.com)'
+    def open_spider(self, spider):
+        try:
+            spider.logger.info("Opening MongoDB pipeline...")
+            self.client = MongoClient('mongodb+srv://xavidb:superman123@serverlessinstance0.lih2lnk.mongodb.net/')
+            self.db = self.client["Xavi_UK"]
+            self.collection_E = self.db['Search_uk_E']
+            spider.logger.info("MongoDB pipeline opened.")
+        except Exception as e:
+            spider.logger.error(f"Error connecting to MongoDB in pipeline: {e}")
 
-# Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+    def close_spider(self, spider):
+        self.client.close()
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 16
-
-# Configure a delay for requests for the same website (default: 0)
-DOWNLOAD_DELAY = 5
-
-# Disable cookies (enabled by default)
-COOKIES_ENABLED = True
-
-# Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
-
-# Override the default request headers:
-DEFAULT_REQUEST_HEADERS = {
-   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-   'Accept-Language': 'en',
-}
-
-# Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'webscraper.middlewares.WebscraperSpiderMiddleware': 543,
-#}
-
-# Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-DOWNLOADER_MIDDLEWARES = {
-    'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 750,
-    'webscraper.middlewares.WebscraperDownloaderMiddleware': 543,
-}
-
-# Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
-
-# Configure item pipelines
-# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'webscraper.pipelines.WebscraperPipeline': 300,
-#}
-
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-AUTOTHROTTLE_DEBUG = False
-
-# Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
-
-# Proxy settings
-PROXY = "http://xavigv:e8qcHlJ5jdHxl7Xj_country-UnitedKingdom@proxy.packetstream.io:31112"
+    def process_item(self, item, spider):
+        try:
+            update_result = self.collection_E.update_one(
+                {'ASIN': item['ASIN']},
+                {'$set': {
+                    'Image URL': item['Image URL'],
+                    'Product Title': item['Product Title'],
+                    'Product Price': item['Product Price'],
+                    'Shipping Fee': item['Shipping Fee']
+                }},
+                upsert=True
+            )
+            if update_result.modified_count > 0:
+                spider.logger.info(f"Data updated for ASIN: {item['ASIN']}")
+            elif update_result.upserted_id is not None:
+                spider.logger.info(f"New document inserted for ASIN: {item['ASIN']}")
+            else:
+                spider.logger.warning(f"No document was updated or inserted for ASIN: {item['ASIN']}, the data might be the same.")
+        except Exception as e:
+            spider.logger.error(f"Error updating MongoDB for ASIN: {item['ASIN']} - {e}")
+        return item
