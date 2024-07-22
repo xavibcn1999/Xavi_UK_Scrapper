63114159dbe1d1ec28f12f7a7ea9a6938ea9c14b
xavibcn1999 <75781897+xavibcn1999@users.noreply.github.com>
2024-07-22 20:56:50 +0100

    Update pipelines.py

--- webscraper/pipelines.py
+++ webscraper/pipelines.py
@@ -2,17 +2,17 @@ import pymongo
 
 class MongoDBPipeline:
 
-    collection_name = 'ebay_items'
-
-    def __init__(self, mongo_uri, mongo_db):
+    def __init__(self, mongo_uri, mongo_db, mongo_collection):
         self.mongo_uri = mongo_uri
         self.mongo_db = mongo_db
+        self.mongo_collection = mongo_collection
 
     @classmethod
     def from_crawler(cls, crawler):
         return cls(
             mongo_uri=crawler.settings.get('MONGO_URI'),
-            mongo_db=crawler.settings.get('MONGO_DATABASE', 'items')
+            mongo_db=crawler.settings.get('MONGO_DATABASE'),
+            mongo_collection=crawler.settings.get('MONGODB_COLLECTION', 'items')
         )
 
     def open_spider(self, spider):
@@ -23,5 +23,5 @@ class MongoDBPipeline:
         self.client.close()
 
     def process_item(self, item, spider):
-        self.db[self.collection_name].insert_one(dict(item))
+        self.db[self.mongo_collection].insert_one(dict(item))
         return item
