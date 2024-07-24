89a0113bd20325cf9299207d73686e8068fad502
xavibcn1999 <75781897+xavibcn1999@users.noreply.github.com>
2024-07-24 17:46:48 +0100

    Update pipelines.py

--- webscraper/pipelines.py
+++ webscraper/pipelines.py
@@ -100,7 +100,7 @@ class MongoDBPipeline:
                 logging.info(f"ROI: {roi}%")
 
                 # Generar la URL de eBay
-                ebay_url = f"https://www.ebay.co.uk/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw={asin}&_sacat=267&LH_TitleDesc=0&_odkw=1492086894&_osacat=267&LH_BIN=1&_sop=15&LH_PrefLoc=1"
+                ebay_url = f"https://www.ebay.co.uk/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw={asin}&_sacat=267&LH_TitleDesc=0&_odkw=1492086894&_osacat=267&LH_BIN=1&_sop=15&LH_PrefLoc=1&rt=nc&LH_ItemCondition=2750%7C4000%7C5000%7C6000%7C10"
 
                 if roi > 0.5:
                     self.send_email(
