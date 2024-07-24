7c1694ad55c613482637b3393d1981b67abd69cd
xavibcn1999 <75781897+xavibcn1999@users.noreply.github.com>
2024-07-24 15:13:46 +0100

    Update pipelines.py

--- webscraper/pipelines.py
+++ webscraper/pipelines.py
@@ -1,114 +1,163 @@
-def calculate_and_send_email(self, item):
-    try:
-        asin = item['nkw']
-        ebay_product_price = item['product_price']
-        ebay_shipping_fee = item['shipping_fee']
-        ebay_price = ebay_product_price + ebay_shipping_fee
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from scrapy.utils.project import get_project_settings
+from pymongo import MongoClient
+import logging
 
-        # Debugging information
-        logging.info(f"Calculando ROI para ASIN: {asin}")
-        logging.info(f"Precio del producto en eBay: {ebay_product_price}")
-        logging.info(f"Costo de envío en eBay: {ebay_shipping_fee}")
-        logging.info(f"Precio de eBay (producto + envío): {ebay_price}")
+class MongoDBPipeline:
+    def __init__(self):
+        settings = get_project_settings()
+        self.mongo_uri = settings.get('MONGO_URI')
+        self.mongo_db = settings.get('MONGO_DATABASE')
+        self.collection_name_e = settings.get('MONGO_COLLECTION_E')
+        self.collection_name_a = settings.get('MONGO_COLLECTION_A')
 
-        amazon_item = self.collection_a.find_one({'ASIN': asin})
-        
-        if amazon_item:
-            logging.info(f"Documento de Amazon recuperado: {amazon_item}")
-            
-            amazon_used_price_str = amazon_item.get('Buy Box Used: 180 days avg.', 0)
-            logging.info(f"Valor extraído de 'Buy Box Used: 180 days avg': {amazon_used_price_str}")
-            
-            if isinstance(amazon_used_price_str, str):
-                try:
-                    amazon_used_price = self.convert_price(amazon_used_price_str)
-                except ValueError as e:
-                    logging.error(f"Error al convertir 'Buy Box Used: 180 days avg' a float: {e}")
-                    amazon_used_price = 0.0
+    def open_spider(self, spider):
+        self.client = MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+        self.collection_e = self.db[self.collection_name_e]
+        self.collection_a = self.db[self.collection_name_a]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        # Convertir los precios a float
+        try:
+            item['product_price'] = self.convert_price(item['product_price'])
+            if item.get('shipping_fee'):
+                item['shipping_fee'] = self.convert_price(item['shipping_fee'])
             else:
-                amazon_used_price = float(amazon_used_price_str)
+                item['shipping_fee'] = 0.0
+        except Exception as e:
+            logging.error(f"Error converting prices: {e}")
+            item['product_price'] = 0.0
+            item['shipping_fee'] = 0.0
 
-            fba_fee_str = amazon_item.get('FBA Fees', 0)
-            if isinstance(fba_fee_str, str):
-                try:
-                    fba_fee = self.convert_price(fba_fee_str)
-                except ValueError as e:
-                    logging.error(f"Error al convertir 'FBA Fees' a float: {e}")
-                    fba_fee = 0.0
-            else:
-                fba_fee = float(fba_fee_str)
+        # Asegurarse de que el item tiene '_id'
+        if '_id' not in item:
+            logging.error("El item no tiene '_id'")
+            return item
 
-            referral_fee_percentage = 0.153 if amazon_used_price > 5 else 0.051
-            referral_fee = amazon_used_price * referral_fee_percentage
+        self.collection_e.update_one({'_id': item['_id']}, {'$set': item}, upsert=True)
+        self.calculate_and_send_email(item)
+        return item
 
-            # Adjusted profit calculation
-            total_cost = ebay_price + fba_fee + referral_fee
-            profit = amazon_used_price - total_cost
-            roi = (profit / total_cost) * 100 if total_cost else 0
+    def convert_price(self, price_str):
+        """Convierte un string de precio a float, eliminando cualquier símbolo adicional."""
+        if isinstance(price_str, str):
+            price_str = price_str.replace('£', '').replace('+', '').replace(',', '').strip()
+        return float(price_str)
 
-            logging.info(f"Precio de venta en Amazon (Buy Box Used): {amazon_used_price}")
-            logging.info(f"Tarifa de FBA: {fba_fee}")
-            logging.info(f"Tarifa de referencia: {referral_fee}")
-            logging.info(f"Ganancia: {profit}")
-            logging.info(f"ROI: {roi}%")
+    def calculate_and_send_email(self, item):
+        try:
+            asin = item['nkw']
+            ebay_product_price = item['product_price']
+            ebay_shipping_fee = item['shipping_fee']
+            ebay_price = ebay_product_price + ebay_shipping_fee
+            logging.info(f"Calculando ROI para ASIN: {asin}")
+            logging.info(f"Precio del producto en eBay: {ebay_product_price}")
+            logging.info(f"Costo de envío en eBay: {ebay_shipping_fee}")
+            logging.info(f"Precio de eBay (producto + envío): {ebay_price}")
 
-            if roi > 50:  # Adjust the ROI threshold as needed
-                ebay_url = self.collection_e.find_one({'_id': item['_id']}).get('url', '')
-                self.send_email(
-                    item['image_url'], ebay_url, ebay_price,
-                    amazon_item.get('Image', ''), amazon_item.get('URL: Amazon', ''), amazon_used_price, roi
-                )
+            amazon_item = self.collection_a.find_one({'ASIN': asin})
+            
+            if amazon_item:
+                logging.info(f"Documento de Amazon recuperado: {amazon_item}")
+                
+                amazon_used_price_str = amazon_item.get('Buy Box Used: 180 days avg.', 0)
+                logging.info(f"Valor extraído de 'Buy Box Used: 180 days avg': {amazon_used_price_str}")
+                
+                if isinstance(amazon_used_price_str, str):
+                    try:
+                        amazon_used_price = self.convert_price(amazon_used_price_str)
+                    except ValueError as e:
+                        logging.error(f"Error al convertir 'Buy Box Used: 180 days avg' a float: {e}")
+                        amazon_used_price = 0.0
+                else:
+                    amazon_used_price = float(amazon_used_price_str)
 
-    except Exception as e:
-        logging.error(f"Error calculating ROI and sending email: {e}")
+                fba_fee_str = amazon_item.get('FBA Fees', 0)
+                if isinstance(fba_fee_str, str):
+                    try:
+                        fba_fee = self.convert_price(fba_fee_str)
+                    except ValueError as e:
+                        logging.error(f"Error al convertir 'FBA Fees' a float: {e}")
+                        fba_fee = 0.0
+                else:
+                    fba_fee = float(fba_fee_str)
 
-def send_email(self, ebay_image, ebay_url, ebay_price, amazon_image, amazon_url, amazon_price, roi):
-    try:
-        sender_email = "xavusiness@gmail.com"
-        receiver_email = "xavialerts@gmail.com"
-        password = "tnthxazpsezagjdc"
+                referral_fee_percentage = 0.153 if amazon_used_price > 5 else 0.051
+                referral_fee = amazon_used_price * referral_fee_percentage
 
-        message = MIMEMultipart("alternative")
-        message["Subject"] = "Alerta de ROI superior al 50%"
-        message["From"] = sender_email
-        message["To"] = receiver_email
+                total_cost = ebay_price + fba_fee + referral_fee
+                profit = amazon_used_price - total_cost
+                roi = (profit / total_cost) * 100 if total_cost else 0
 
-        text = f"""\
-        Alerta de ROI superior al 50%:
-        - Imagen de eBay: {ebay_image}
-        - URL de eBay: {ebay_url}
-        - Precio de eBay: £{ebay_price}
-        - Imagen de Amazon: {amazon_image}
-        - URL de Amazon: {amazon_url}
-        - Precio de Amazon: £{amazon_price}
-        - ROI: {roi}%
-        """
+                logging.info(f"Precio de venta en Amazon (Buy Box Used): {amazon_used_price}")
+                logging.info(f"Tarifa de FBA: {fba_fee}")
+                logging.info(f"Tarifa de referencia: {referral_fee}")
+                logging.info(f"Ganancia: {profit}")
+                logging.info(f"ROI: {roi}%")
 
-        html = f"""\
-        <html>
-          <body>
-            <h2>Alerta de ROI superior al 50%</h2>
-            <p><strong>Imagen de eBay:</strong> <img src="{ebay_image}" width="100"></p>
-            <p><strong>URL de eBay:</strong> <a href="{ebay_url}">{ebay_url}</a></p>
-            <p><strong>Precio de eBay:</strong> £{ebay_price}</p>
-            <p><strong>Imagen de Amazon:</strong> <img src="{amazon_image}" width="100"></p>
-            <p><strong>URL de Amazon:</strong> <a href="{amazon_url}">{amazon_url}</a></p>
-            <p><strong>Precio de Amazon:</strong> £{amazon_price}</p>
-            <p><strong>ROI:</strong> {roi}%</p>
-          </body>
-        </html>
-        """
+                if roi > 50:  # Adjust the ROI threshold as needed
+                    ebay_url = self.collection_e.find_one({'_id': item['_id']}).get('url', '')
+                    self.send_email(
+                        item['image_url'], ebay_url, ebay_price,
+                        amazon_item.get('Image', ''), amazon_item.get('URL: Amazon', ''), amazon_used_price, roi
+                    )
 
-        part1 = MIMEText(text, "plain")
-        part2 = MIMEText(html, "html")
+        except Exception as e:
+            logging.error(f"Error calculating ROI and sending email: {e}")
 
-        message.attach(part1)
-        message.attach(part2)
+    def send_email(self, ebay_image, ebay_url, ebay_price, amazon_image, amazon_url, amazon_price, roi):
+        try:
+            sender_email = "xavusiness@gmail.com"
+            receiver_email = "xavialerts@gmail.com"
+            password = "tnthxazpsezagjdc"
 
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-            server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, message.as_string())
+            message = MIMEMultipart("alternative")
+            message["Subject"] = "Alerta de ROI superior al 50%"
+            message["From"] = sender_email
+            message["To"] = receiver_email
 
-        logging.info("Email enviado exitosamente")
-    except Exception as e:
-        logging.error(f"Error al enviar email: {e}")
+            text = f"""\
+            Alerta de ROI superior al 50%:
+            - Imagen de eBay: {ebay_image}
+            - URL de eBay: {ebay_url}
+            - Precio de eBay: £{ebay_price}
+            - Imagen de Amazon: {amazon_image}
+            - URL de Amazon: {amazon_url}
+            - Precio de Amazon: £{amazon_price}
+            - ROI: {roi}%
+            """
+
+            html = f"""\
+            <html>
+              <body>
+                <h2>Alerta de ROI superior al 50%</h2>
+                <p><strong>Imagen de eBay:</strong> <img src="{ebay_image}" width="100"></p>
+                <p><strong>URL de eBay:</strong> <a href="{ebay_url}">{ebay_url}</a></p>
+                <p><strong>Precio de eBay:</strong> £{ebay_price}</p>
+                <p><strong>Imagen de Amazon:</strong> <img src="{amazon_image}" width="100"></p>
+                <p><strong>URL de Amazon:</strong> <a href="{amazon_url}">{amazon_url}</a></p>
+                <p><strong>Precio de Amazon:</strong> £{amazon_price}</p>
+                <p><strong>ROI:</strong> {roi}%</p>
+              </body>
+            </html>
+            """
+
+            part1 = MIMEText(text, "plain")
+            part2 = MIMEText(html, "html")
+
+            message.attach(part1)
+            message.attach(part2)
+
+            with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+                server.login(sender_email, password)
+                server.sendmail(sender_email, receiver_email, message.as_string())
+
+            logging.info("Email enviado exitosamente")
+        except Exception as e:
+            logging.error(f"Error al enviar email: {e}")
