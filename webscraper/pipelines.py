f95b88d31a9c0c4b26448515b8f2e9219c78db68
xavibcn1999 <75781897+xavibcn1999@users.noreply.github.com>
2024-07-24 10:34:26 +0100

    Update pipelines.py

--- webscraper/pipelines.py
+++ webscraper/pipelines.py
@@ -10,8 +10,8 @@ class MongoDBPipeline:
         settings = get_project_settings()
         self.mongo_uri = settings.get('MONGO_URI')
         self.mongo_db = settings.get('MONGO_DATABASE')
-        self.collection_name_e = settings.get('MONGODB_COLLECTION_E')
-        self.collection_name_a = settings.get('MONGODB_COLLECTION_A')
+        self.collection_name_e = settings.get('MONGO_COLLECTION_E')
+        self.collection_name_a = settings.get('MONGO_COLLECTION_A')
 
     def open_spider(self, spider):
         self.client = MongoClient(self.mongo_uri)
@@ -34,17 +34,15 @@ class MongoDBPipeline:
 
             amazon_item = self.collection_a.find_one({'ASIN': asin})
             if amazon_item:
-                amazon_used_price_str = amazon_item.get('Buy Box Used: 180 days avg', '0')
-                amazon_used_price = float(amazon_used_price_str.replace('£', '').replace(',', '').strip())
-                fba_fee_str = amazon_item.get('FBA Fees:', '0')
-                fba_fee = float(fba_fee_str.replace('£', '').replace(',', '').strip())
+                amazon_used_price = float(amazon_item.get('Buy Box Used: 180 days avg', '0').replace('£', '').replace(',', '').strip())
+                fba_fee = float(amazon_item.get('FBA Fees:', '0').replace('£', '').replace(',', '').strip())
                 referral_fee_percentage = 0.153 if amazon_used_price > 5 else 0.051
                 referral_fee = amazon_used_price * referral_fee_percentage
 
                 profit = ebay_price - amazon_used_price - fba_fee - referral_fee
-                roi = (profit / ebay_price) * 100 if ebay_price else 0
+                roi = profit / ebay_price if ebay_price else -1  # Cambiado 0 por -1
 
-                if roi > 50:
+                if roi > 0.5:
                     self.send_email(
                         item['image_url'], item['product_title'], ebay_price,
                         amazon_item.get('Image', ''), amazon_item.get('URL: Amazon', ''), amazon_used_price, roi
@@ -57,49 +55,4 @@ class MongoDBPipeline:
         try:
             sender_email = "xavusiness@gmail.com"
             receiver_email = "xavialerts@gmail.com"
-            password = "tnthxazpsezagjdc"
-
-            message = MIMEMultipart("alternative")
-            message["Subject"] = "Alerta de ROI superior al 50%"
-            message["From"] = sender_email
-            message["To"] = receiver_email
-
-            text = f"""\
-            Alerta de ROI superior al 50%:
-            - Imagen de eBay: {ebay_image}
-            - URL de eBay: {ebay_url}
-            - Precio de eBay: £{ebay_price}
-            - Imagen de Amazon: {amazon_image}
-            - URL de Amazon: {amazon_url}
-            - Precio de Amazon: £{amazon_price}
-            - ROI: {roi}%
-            """
-
-            html = f"""\
-            <html>
-              <body>
-                <h2>Alerta de ROI superior al 50%</h2>
-                <p><strong>Imagen de eBay:</strong> <img src="{ebay_image}" width="100"></p>
-                <p><strong>URL de eBay:</strong> <a href="{ebay_url}">{ebay_url}</a></p>
-                <p><strong>Precio de eBay:</strong> £{ebay_price}</p>
-                <p><strong>Imagen de Amazon:</strong> <img src="{amazon_image}" width="100"></p>
-                <p><strong>URL de Amazon:</strong> <a href="{amazon_url}">{amazon_url}</a></p>
-                <p><strong>Precio de Amazon:</strong> £{amazon_price}</p>
-                <p><strong>ROI:</strong> {roi}%</p>
-              </body>
-            </html>
-            """
-
-            part1 = MIMEText(text, "plain")
-            part2 = MIMEText(html, "html")
-
-            message.attach(part1)
-            message.attach(part2)
-
-            with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-                server.login(sender_email, password)
-                server.sendmail(sender_email, receiver_email, message.as_string())
-
-            logging.info("Email enviado exitosamente")
-        except Exception as e:
-            logging.error(f"Error al enviar email: {e}")
+           
