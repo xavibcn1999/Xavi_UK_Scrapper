f323ad7cbcb83474cbf99b44d955f0fd409752d0
xavibcn1999 <75781897+xavibcn1999@users.noreply.github.com>
2024-07-24 10:24:39 +0100

    Update pipelines.py

--- webscraper/pipelines.py
+++ webscraper/pipelines.py
@@ -10,8 +10,8 @@ class MongoDBPipeline:
         settings = get_project_settings()
         self.mongo_uri = settings.get('MONGO_URI')
         self.mongo_db = settings.get('MONGO_DATABASE')
-        self.collection_name_e = settings.get('MONGO_COLLECTION_E')
-        self.collection_name_a = settings.get('MONGO_COLLECTION_A')
+        self.collection_name_e = settings.get('MONGODB_COLLECTION_E')
+        self.collection_name_a = settings.get('MONGODB_COLLECTION_A')
 
     def open_spider(self, spider):
         self.client = MongoClient(self.mongo_uri)
@@ -34,18 +34,17 @@ class MongoDBPipeline:
 
             amazon_item = self.collection_a.find_one({'ASIN': asin})
             if amazon_item:
-                amazon_used_price = amazon_item.get('Buy Box Used: 180 days avg', '0').replace('£', '').replace(',', '').strip()
-                amazon_used_price = float(amazon_used_price) if amazon_used_price else 0.0
-                fba_fee = amazon_item.get('FBA Fees:', '0').replace('£', '').replace(',', '').strip()
-                fba_fee = float(fba_fee) if fba_fee else 0.0
-
+                amazon_used_price_str = amazon_item.get('Buy Box Used: 180 days avg', '0')
+                amazon_used_price = float(amazon_used_price_str.replace('£', '').replace(',', '').strip())
+                fba_fee_str = amazon_item.get('FBA Fees:', '0')
+                fba_fee = float(fba_fee_str.replace('£', '').replace(',', '').strip())
                 referral_fee_percentage = 0.153 if amazon_used_price > 5 else 0.051
                 referral_fee = amazon_used_price * referral_fee_percentage
 
                 profit = ebay_price - amazon_used_price - fba_fee - referral_fee
-                roi = profit / ebay_price if ebay_price else -1  # -1 si ebay_price es cero
+                roi = (profit / ebay_price) * 100 if ebay_price else 0
 
-                if roi > 0.5:
+                if roi > 50:
                     self.send_email(
                         item['image_url'], item['product_title'], ebay_price,
                         amazon_item.get('Image', ''), amazon_item.get('URL: Amazon', ''), amazon_used_price, roi
@@ -73,7 +72,7 @@ class MongoDBPipeline:
             - Imagen de Amazon: {amazon_image}
             - URL de Amazon: {amazon_url}
             - Precio de Amazon: £{amazon_price}
-            - ROI: {roi * 100}%
+            - ROI: {roi}%
             """
 
             html = f"""\
@@ -86,7 +85,7 @@ class MongoDBPipeline:
                 <p><strong>Imagen de Amazon:</strong> <img src="{amazon_image}" width="100"></p>
                 <p><strong>URL de Amazon:</strong> <a href="{amazon_url}">{amazon_url}</a></p>
                 <p><strong>Precio de Amazon:</strong> £{amazon_price}</p>
-                <p><strong>ROI:</strong> {roi * 100}%</p>
+                <p><strong>ROI:</strong> {roi}%</p>
               </body>
             </html>
             """
