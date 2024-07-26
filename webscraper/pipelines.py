ac063ab0d8805a63193a351be9b9aeba5f6ee02d
xavibcn1999 <75781897+xavibcn1999@users.noreply.github.com>
2024-07-26 14:55:28 +0100

    Update pipelines.py

--- webscraper/pipelines.py
+++ webscraper/pipelines.py
@@ -38,32 +38,32 @@ class MongoDBPipeline:
         self.client.close()
 
     def process_item(self, item, spider):
-    try:
-        item['product_price'] = self.convert_price(item['product_price'])
-        item['shipping_fee'] = self.convert_price(item['shipping_fee']) if item.get('shipping_fee') else 0.0
-    except Exception as e:
-        logging.error(f"Error converting prices: {e}")
-        item['product_price'] = 0.0
-        item['shipping_fee'] = 0.0
+        try:
+            item['product_price'] = self.convert_price(item['product_price'])
+            item['shipping_fee'] = self.convert_price(item['shipping_fee']) if item.get('shipping_fee') else 0.0
+        except Exception as e:
+            logging.error(f"Error converting prices: {e}")
+            item['product_price'] = 0.0
+            item['shipping_fee'] = 0.0
 
-    if '_id' not in item:
-        logging.error("El item no tiene '_id'")
+        if '_id' not in item:
+            logging.error("El item no tiene '_id'")
+            return item
+
+        # Ensure item_number and product_url are in item and not empty
+        item['item_number'] = item.get('item_number', '')
+        item['product_url'] = item.get('product_url', '')
+
+        # Update item in the Search_uk_E collection
+        self.collection_e.update_one({'_id': item['_id']}, {'$set': item}, upsert=True)
+
+        # Add item number to the Search_uk_E collection
+        self.collection_e.update_one({'_id': item['_id']}, {'$set': {'item_number': item.get('item_number')}}, upsert=True)
+
+        # Calculate and potentially send email
+        self.calculate_and_send_email(item)
+
         return item
-
-    # Ensure item_number and product_url are in item and not empty
-    item['item_number'] = item.get('item_number', '')
-    item['product_url'] = item.get('product_url', '')
-
-    # Update item in the Search_uk_E collection
-    self.collection_e.update_one({'_id': item['_id']}, {'$set': item}, upsert=True)
-
-    # Add item number to the Search_uk_E collection
-    self.collection_e.update_one({'_id': item['_id']}, {'$set': {'item_number': item.get('item_number')}}, upsert=True)
-
-    # Calculate and potentially send email
-    self.calculate_and_send_email(item)
-
-    return item
 
     def convert_price(self, price_str):
         if isinstance(price_str, str):
