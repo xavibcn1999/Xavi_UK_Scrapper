eb37369d61017d8e7e262a151667a7e47895a841
xavibcn1999 <75781897+xavibcn1999@users.noreply.github.com>
2024-08-01 07:34:45 +0100

    Update ebay_top2.py

--- webscraper/spiders/ebay_top2.py
+++ webscraper/spiders/ebay_top2.py
@@ -51,30 +51,30 @@ class EbayTop2Spider(scrapy.Spider):
 
     def connect(self):
         try:
-            self.logger.info("Attempting to connect to MongoDB...")
+            # self.logger.info("Attempting to connect to MongoDB...")
             client = MongoClient('mongodb+srv://xavidb:superman123@serverlessinstance0.lih2lnk.mongodb.net/Xavi_UK?retryWrites=true&w=majority')
             self.db = client["Xavi_UK"]
             self.collection_E = self.db['Search_uk_E']
             self.ebay_items_collection = self.db['ebay_items']
-            self.logger.info("Connected to MongoDB.")
+            # self.logger.info("Connected to MongoDB.")
         except Exception as e:
-            self.logger.error(f"Error connecting to MongoDB: {e}")
+            # self.logger.error(f"Error connecting to MongoDB: {e}")
 
     def start_requests(self):
         self.logger.info("Fetching URLs from the Search_uk_E collection...")
         try:
             # Limpiar la colección ebay_items
-            self.logger.info("Clearing ebay_items collection...")
+            # self.logger.info("Clearing ebay_items collection...")
             self.ebay_items_collection.delete_many({})
 
             data_urls = list(self.collection_E.find({'url': {'$ne': ''}}))
-            self.logger.info(f"Found {len(data_urls)} URLs to process.")
+            # self.logger.info(f"Found {len(data_urls)} URLs to process.")
         except Exception as e:
-            self.logger.error(f"Error fetching URLs from MongoDB: {e}")
+            # self.logger.error(f"Error fetching URLs from MongoDB: {e}")
             data_urls = []
 
         if not data_urls:
-            self.logger.warning("No URLs found in the Search_uk_E collection.")
+            # self.logger.warning("No URLs found in the Search_uk_E collection.")
             return
 
         for data_urls_loop in data_urls:
@@ -82,7 +82,7 @@ class EbayTop2Spider(scrapy.Spider):
             search_key = data_urls_loop.get('search_key', '')
 
             if url:
-                self.logger.info(f"Creating request for URL: {url}")
+                # self.logger.info(f"Creating request for URL: {url}")
                 yield scrapy.Request(
                     url=url,
                     callback=self.parse,
@@ -91,19 +91,19 @@ class EbayTop2Spider(scrapy.Spider):
                     errback=self.errback_httpbin
                 )
             else:
-                self.logger.warning("Empty URL found in the Search_uk_E collection.")
+                # self.logger.warning("Empty URL found in the Search_uk_E collection.")
 
     def parse(self, response):
         _id = response.meta.get('_id')
         search_key = response.meta.get('search_key')
-        self.logger.info(f"Processing response for _id: {_id}")
+        # self.logger.info(f"Processing response for _id: {_id}")
         listings = response.xpath('//ul//div[@class="s-item__wrapper clearfix"]')
 
         count = 0
 
         for listing in listings:
             if listing.xpath('.//li[contains(@class,"srp-river-answer--REWRITE_START")]').get():
-                self.logger.info("Found international sellers separator. Stopping extraction for this URL.")
+                # self.logger.info("Found international sellers separator. Stopping extraction for this URL.")
                 break
 
             if listing.xpath('.//span[@class="s-item__location s-item__itemLocation"]').get():
@@ -132,7 +132,7 @@ class EbayTop2Spider(scrapy.Spider):
             except:
                 item_number = ''
 
-            self.logger.info(f"Extracted data - Link: {link}, Title: {title}, Price: {price}, Image: {image}, Shipping Cost: {shipping_cost}, Item Number: {item_number}")
+            # self.logger.info(f"Extracted data - Link: {link}, Title: {title}, Price: {price}, Image: {image}, Shipping Cost: {shipping_cost}, Item Number: {item_number}")
 
             item = {
                 'image_url': image,
@@ -154,20 +154,20 @@ class EbayTop2Spider(scrapy.Spider):
                 break
 
         if count == 0:
-            self.logger.warning(f"No valid listings found for _id: {_id}")
+            # self.logger.warning(f"No valid listings found for _id: {_id}")
 
     def errback_httpbin(self, failure):
-        self.logger.error(repr(failure))
+        # self.logger.error(repr(failure))
         if failure.check(HttpError):
             response = failure.value.response
-            self.logger.error('HttpError on %s', response.url)
+            # self.logger.error('HttpError on %s', response.url)
             if response.status == 503:
-                self.logger.warning('503 Service Unavailable on %s', response.url)
+                # self.logger.warning('503 Service Unavailable on %s', response.url)
                 yield scrapy.Request(response.url, callback=self.parse, dont_filter=True, headers=self.headers, meta={'proxy': self.proxy})
 
         elif failure.check(DNSLookupError):
             request = failure.request
-            self.logger.error('DNSLookupError on %s', request.url)
+            # self.logger.error('DNSLookupError on %s', request.url)
         elif failure.check(TimeoutError, TCPTimedOutError):
             request = failure.request
-            self.logger.error('TimeoutError on %s', request.url)
+            # self.logger.error('TimeoutError on %s', request.url)
