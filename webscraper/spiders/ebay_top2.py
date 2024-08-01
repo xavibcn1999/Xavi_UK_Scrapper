118bb2319c6a1abb13424f0ceefc6b3dba0d72a2
xavibcn1999 <75781897+xavibcn1999@users.noreply.github.com>
2024-08-01 07:15:16 +0100

    Update ebay_top2.py

--- webscraper/spiders/ebay_top2.py
+++ webscraper/spiders/ebay_top2.py
@@ -36,7 +36,7 @@ class EbayTop2Spider(scrapy.Spider):
     headers = {
         'User-Agent': header.generate()['User-Agent'],
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Language': 'en-GB,en;q=0.9',  # Preferencia por inglés británico
         'Accept-Encoding': 'gzip, deflate, br',
         'Connection': 'keep-alive',
         'Upgrade-Insecure-Requests': '1',
